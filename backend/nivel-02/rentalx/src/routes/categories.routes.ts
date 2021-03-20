446c9a1f0db5f8c3a4c34bd35f6209d65cca35be
diasisac <isac@mobilesaude.com>
2021-03-20 15:22:33 -0300

    Criando o use case para importar categorias

--- backend/nivel-02/rentalx/src/routes/categories.routes.ts
+++ backend/nivel-02/rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import { importCategoryController} from '../modules/cars/useCases/importCategory';
 import multer from "multer";
 
 const categoriesRoutes = Router();
@@ -18,9 +19,8 @@ categoriesRoutes.get("/", (request, response) => {
 });
 
 categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
-    const { file } = request;
-    console.log(file);
-    return response.send();
+
+    return importCategoryController.handle(request, response);
 });
 
 export {categoriesRoutes};
