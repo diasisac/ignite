cbab1b4e6671c142b990931960274c7fb92f81f6
diasisac <isac@mobilesaude.com>
2021-03-20 14:48:43 -0300

    Refatorando as rotas

--- backend/nivel-02/rentalx/src/server.ts
+++ backend/nivel-02/rentalx/src/server.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 
+import { router } from './routes';
+
 import {categoriesRoutes}  from './routes/categories.routes';
 import {specificationsRoutes}  from './routes/specifications.routes';
 
@@ -7,7 +9,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/categories",categoriesRoutes);
-app.use("/specifications",specificationsRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Server is runing!"));
