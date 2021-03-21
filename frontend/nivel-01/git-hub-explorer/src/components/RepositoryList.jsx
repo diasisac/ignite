1e1d7e0b4f78b8042dbc994f30e1cc30cb8ae380
diasisac <isac@mobilesaude.com>
2021-03-21 14:38:42 -0300

    Utilizando useEffect

--- frontend/nivel-01/git-hub-explorer/src/components/RepositoryList.jsx
+++ frontend/nivel-01/git-hub-explorer/src/components/RepositoryList.jsx
@@ -1,5 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
+
 const repository = {
     name: 'uniform',
     description: 'Forms in react',
@@ -7,6 +11,14 @@ const repository = {
 }
 
 export function RepositoryList(){
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(()=> {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, [])
 
     return (
         <section className="repository-list">
