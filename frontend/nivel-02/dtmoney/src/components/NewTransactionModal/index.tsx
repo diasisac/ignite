6cee00d3da99721b1f049ed83c3042f684b96c7a
diasisac <isac@mobilesaude.com>
2021-03-23 20:06:55 -0300

    Cores dos botoes

--- frontend/nivel-02/dtmoney/src/components/NewTransactionModal/index.tsx
+++ frontend/nivel-02/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -44,6 +44,7 @@ export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModa
                   type="button"
                   onClick={() => {setType('deposit');}}
                   isActive={type==='deposit'}
+                  activeColor="green"
                   >
                       <img src={incomeImg} alt=""/>
                       <span>Entrada</span>
@@ -53,6 +54,7 @@ export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModa
                   type="button"
                   onClick={() => {setType('withdraw');}}
                   isActive={type==='withdraw'}
+                  activeColor="red"
                   >
                       <img src={outcomeImg} alt=""/>
                       <span>Saída</span>
