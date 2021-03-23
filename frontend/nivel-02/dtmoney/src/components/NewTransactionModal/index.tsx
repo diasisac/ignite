e34a0557a4259e4bbb52e6404065c5cac4cd64d8
diasisac <isac@mobilesaude.com>
2021-03-23 20:48:30 -0300

    Salvando dados do form

--- frontend/nivel-02/dtmoney/src/components/NewTransactionModal/index.tsx
+++ frontend/nivel-02/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,7 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import closeImg from '../../assets/close.svg';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
 
 interface NewTransactionModalProps{
     isOpen:boolean;
@@ -11,8 +11,24 @@ interface NewTransactionModalProps{
 }
 
 export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModalProps){
-    
+
+    const [title, setTitle] = useState('');
+    const [value, setValue] = useState(0);
+    const [category, setCategory] = useState('');
     const [type, setType] = useState('deposit');
+
+    function handleCreateNewTransaction(event: FormEvent){
+        event.preventDefault();
+
+
+        console.log({
+            title,
+            value,
+            type,
+            category
+        }
+        )
+    }
 
     return (
         <Modal 
@@ -26,16 +42,20 @@ export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModa
                 <img src={closeImg} alt="Fechar modal"/>
             </button>
 
-            <Container>
+            <Container onSubmit={handleCreateNewTransaction}>
                 <h2>Cadastrar transação</h2>
 
                 <input 
                 placeholder="Titúlo"
+                value={title}
+                onChange={event => setTitle(event.target.value)}
                 ></input>
 
                 <input 
                 type="number" 
                 placeholder="Valor"
+                value={value}
+                onChange={event => setValue(Number(event.target.value))}
                 >
                 </input>
 
@@ -63,6 +83,8 @@ export function NewTransactionModal({isOpen,onRequestClose }: NewTransactionModa
 
                 <input
                 placeholder="categoria"
+                value={category}
+                onChange={event => setCategory(event.target.value)}
                 >
                 </input>
 
