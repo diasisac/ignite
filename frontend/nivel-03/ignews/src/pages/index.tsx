9697ab540634e20a9bda77a735654e7f2e98d6a5
diasisac <isac@mobilesaude.com>
2021-03-31 17:07:28 -0300

    Pagina home

--- frontend/nivel-03/ignews/src/pages/index.tsx
+++ frontend/nivel-03/ignews/src/pages/index.tsx
@@ -1,16 +1,24 @@
-import styles from "../styles/home.module.scss";
+import styles from "./home.module.scss";
 import Head from 'next/head';
 
 export default function Home() {
   return (
     <>
     <Head>
-      <title>ig.news</title>
+      <title>Home | ig.news</title>
       </Head>
-      <h1>
-      
-        Hello Word
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News abount the <span>React</span>word.</h1>
+          <p>
+            Get access to all the publications <br/>
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding"/>
+      </main>
     </>
   )
 }
