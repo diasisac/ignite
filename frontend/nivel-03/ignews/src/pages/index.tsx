909b561055a126db7cd06f85e0016288b0231142
diasisac <isac@mobilesaude.com>
2021-04-02 13:02:27 -0300

    Static Site Generation (SSG)

--- frontend/nivel-03/ignews/src/pages/index.tsx
+++ frontend/nivel-03/ignews/src/pages/index.tsx
@@ -1,6 +1,6 @@
 import styles from "./home.module.scss";
 import Head from 'next/head';
-import { GetServerSideProps} from 'next';
+import { GetServerSideProps, GetStaticProps} from 'next';
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
 
@@ -34,7 +34,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async() =>{
+export const getStaticProps: GetStaticProps = async() =>{
   
   const price = await stripe.prices.retrieve('price_1IbAhICZiXzPsApAJCvYBLHf')
 
@@ -49,6 +49,7 @@ export const getServerSideProps: GetServerSideProps = async() =>{
   return {
     props:{
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, //24 hours
   }
 }
