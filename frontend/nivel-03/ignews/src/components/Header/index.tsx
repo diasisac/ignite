bef7556b03cda9ff89b3803935f8ac5a61ea922f
diasisac <isac@mobilesaude.com>
2021-04-06 14:25:49 -0300

    Componente: ActiveLink

--- frontend/nivel-03/ignews/src/components/Header/index.tsx
+++ frontend/nivel-03/ignews/src/components/Header/index.tsx
@@ -1,6 +1,8 @@
 import { SignIButton } from '../SignInButton';
 import styles from './styles.module.scss';
 import Link from 'next/link';
+import { useRouter } from 'next/dist/client/router';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header(){
     return(
@@ -8,12 +10,12 @@ export function Header(){
             <div className={styles.headerContent}>
                 <img src="/images/logo.svg" alt="ig.news"/>
                     <nav>
-                        <Link href="/">
-                            <a className={styles.active}>Home</a>
-                        </Link>
-                        <Link href="/posts">
+                        <ActiveLink activeClassName={styles.active} href="/">
+                            <a>Home</a>
+                        </ActiveLink>
+                        <ActiveLink activeClassName={styles.active} href="/posts">
                             <a>Posts</a>
-                        </Link>
+                        </ActiveLink>
                     </nav>
                     <SignIButton/>
             </div> 
